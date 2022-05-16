80d149d429eea6e8d3257c6dc7cbaf51c0949bd2
noa-seneor <100723117+noa-seneor@users.noreply.github.com>
2022-05-16 08:20:15 +0200

    added exercice 9

---
+++ np9.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+
+def multiply_arrays(array1, array2):
+    """
+    Function that multiply two given arrays of same size element-by-element
+    :return: multiplied Array
+    """
+    return np.multiply(array1, array2)
+
+
+
