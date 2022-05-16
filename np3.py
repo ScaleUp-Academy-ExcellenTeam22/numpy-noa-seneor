dfe473c878673f859a2289feb6656ef90b7f9a49
noa-seneor <100723117+noa-seneor@users.noreply.github.com>
2022-05-16 08:15:06 +0200

    added exercice 3

---
+++ np3.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+
+def row_columns(matrix):
+    """
+    Function that find number of rows and columns of given matrix
+    :param matrix:
+    :return: (rows, columns)
+    """
+    return matrix.shape
+
+
+
+
