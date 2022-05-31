abe761c93b1e1278ee4606d8298b91ed9dd98cde
noa-seneor <100723117+noa-seneor@users.noreply.github.com>
2022-05-31 13:40:15 +0200

    Update np17.py

--- np17.py
+++ np17.py
@@ -1,13 +1,8 @@
 import numpy as np
 
 
-def median_array(array):
+def median_array(array) -> float:
     """
-    Function that compute the median of flattened given array
+    Function that computes the median of flattened given array
     """
     return np.median(array)
-
-
-if __name__ == '__main__':
-    arr = np.array([[0, 1, 2, 3, 4, 5], [6, 7, 8, 9, 10, 11]])
-    print(median_array(arr))
