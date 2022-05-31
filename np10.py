fbaacd4cf3a441b3d222948202bd8cf3a613fcb2
noa-seneor <100723117+noa-seneor@users.noreply.github.com>
2022-05-31 12:59:47 +0200

    Update np10.py

--- np10.py
+++ np10.py
@@ -1,18 +1,10 @@
 import numpy as np
 
 
-def sort_axis(array):
+def sort_axis(array) -> tuple:
     """
-    Function that sort an along the first, last axis of a given array
+    Function that sorts along the first, last axis of a given array
     """
     first_axis = np.sort(array, axis=0)
-    print(f"sorted by 1st axis: \n {first_axis}")
     last_axis = np.sort(first_axis, axis=1)
-    print(f"sorted by last axis: \n {last_axis}")
-
-
-if __name__ == '__main__':
-    sort_axis(np.array([[4, 6], [2, 1]]))
-
-
-
+    return first_axis, last_axis
