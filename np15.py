491b678289b751a9ef0eeed117e65eab0290a8d6
noa-seneor <100723117+noa-seneor@users.noreply.github.com>
2022-05-31 13:32:28 +0200

    Update np15.py

--- np15.py
+++ np15.py
@@ -1,14 +1,10 @@
 import numpy as np
 
 
-def create_array():
+def create_0_255_array() -> np.array:
     """
-    Function that create a three-dimension array with shape (300,400,5) and
+    Function that creates a three-dimension array with shape (300,400,5) and
     fill the array elements with value between 0 and 255
     """
     array = np.random.randint(0, 256, (300, 400, 5))
-    print(array)
-
-
-if __name__ == '__main__':
-    create_array()
+    return array
