7aad11092f43e0406c95f6e7ff1c858a965394d9
noa-seneor <100723117+noa-seneor@users.noreply.github.com>
2022-05-31 10:42:08 +0200

    Update np6.py

--- np6.py
+++ np6.py
@@ -1,18 +1,15 @@
 import matplotlib.pyplot as mplot
 import numpy as np
 
-
-def plot_sin(x):
+def plot_sin(x) -> None:
     """
-    Function that compute the x and y coordinates for points
+    Function that computes the x and y coordinates for points
     on a sine curve and plot the points using matplotlib
-    :return: Vector
     """
     y = np.sin(x)
     mplot.plot(x, y)
-    return mplot.show()
+    mplot.show()
 
 
-if __name__ == '__main__':
-    print(plot_sin(np.arange(0, 3*np.pi, 0.2)))
+plot_sin(np.arange(0, 3*np.pi, 0.2))
 
