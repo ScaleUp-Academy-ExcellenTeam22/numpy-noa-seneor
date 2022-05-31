8caf04717bf3da1ea0a784e77da6e9b7f8ccd0cb
noa-seneor <100723117+noa-seneor@users.noreply.github.com>
2022-05-31 13:01:48 +0200

    Update np13.py

--- np13.py
+++ np13.py
@@ -1,17 +1,13 @@
 import numpy as np
 
 
-def convert_2d(arr):
+def convert_2d(arr) -> np.array:
     """
-    Function that convert (in sequence depth wise (along third axis))
+    Function that converts (in sequence depth wise (along third axis))
     two 1-D arrays into a 2-D array.
     """
     array = np.dstack((arr[0], arr[1]))
     return array
 
 
-if __name__ == '__main__':
-    a = np.array([10, 20, 30])
-    b = np.array([40, 50, 60])
-    print(convert_2d(np.array([a, b])))
 
