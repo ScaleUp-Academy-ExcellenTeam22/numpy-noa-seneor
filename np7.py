1b9b5e3ecaf07701f748d4d79fda132f75e8f854
noa-seneor <100723117+noa-seneor@users.noreply.github.com>
2022-05-31 12:44:35 +0200

    Update np7.py

--- np7.py
+++ np7.py
@@ -1,18 +1,12 @@
 import numpy as np
 
 
-def swap_last_first():
+def swap_last_first() -> np.array:
     """
-    Function that create a 4x4 array with random values,
-    then create a new array from the said array swapping first and last rows
-    :return: Matrix
+    Function that creates a 4x4 array with random values,
+    then returns a new array from the said array swapping first and last rows
     """
-    matrix = np.arange(16).reshape(-1, 4)
-    print(f"before:\n {matrix}")
+    matrix = np.random.random((4, 4))
     new_matrix = np.vstack([matrix[matrix.shape[0]-1], matrix[1:3], matrix[0]])
-    print(f"after:\n {new_matrix}")
-
-
-if __name__ == '__main__':
-    print(swap_last_first())
+    return new_matrix
 
