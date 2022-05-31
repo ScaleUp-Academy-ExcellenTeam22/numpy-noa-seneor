1143f71f81f8733dbe218f4b725256dae548a3a3
noa-seneor <100723117+noa-seneor@users.noreply.github.com>
2022-05-31 13:03:35 +0200

    Update np14.py

--- np14.py
+++ np14.py
@@ -1,16 +1,12 @@
 import numpy as np
 
 
-def combine_1d2d(one_dim, two_dim):
+def combine_1d2d(one_dim, two_dim) -> None:
     """
-    Function that combine a one and a two dimensional array together
-     and display their elements.
+    Function that combines a one and a two dimensional array together
+    and display their elements.
     """
     for a, b in np.nditer([one_dim,two_dim]):
         print(f" {a}:{b}")
 
 
-if __name__ == '__main__':
-    a = np.array([0, 1, 2, 3])
-    b = np.array([[0, 1, 2, 3], [4, 5, 6, 7]])
-    combine_1d2d(a, b)
