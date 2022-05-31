1e9541c03dd21c612f490c7beb3c095e1d067a7b
noa-seneor <100723117+noa-seneor@users.noreply.github.com>
2022-05-31 12:54:07 +0200

    Update np11.py

--- np11.py
+++ np11.py
@@ -1,16 +1,15 @@
 import numpy as np
 
 
-def diagonal_array():
+def diagonal_array() -> np.array:
     """
-    Function that create a 3-D array with ones on a diagonal and zeros elsewhere
+    Function that returns a 3-D array with ones on a diagonal and zeros elsewhere
     """
     array = np.zeros((3, 3))
     np.fill_diagonal(array, 1, wrap=True)
     return array
 
 
-if __name__ == '__main__':
-    print(diagonal_array())
+
 
 
