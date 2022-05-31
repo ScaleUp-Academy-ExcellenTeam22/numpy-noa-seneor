a707c96b576c161f71318c9b6df093406ec9557b
noa-seneor <100723117+noa-seneor@users.noreply.github.com>
2022-05-31 10:26:48 +0200

    Update np4.py

--- np4.py
+++ np4.py
@@ -1,18 +1,12 @@
 import numpy as np
 
 
-def border_matrix():
+def border_matrix() -> np.array:
     """
-    Function that create a 10x10 matrix where elements in the border equals to 1
-    the rest 0
-    :return: matrix
+    Function that returns a 10d array with 1 in the border and 0 inside
     """
-    matrix = np.random.randint(0, 1, 100)
-    matrix = matrix.reshape((10, 10))
-    matrix[0] = 1
-    matrix[9] = 1
-    matrix[:, 0] = 1
-    matrix[:, 9] = 1
+    matrix =np.ones((10,10))
+    matrix[1:-1,1:-1] = 0
     return matrix
 
 
