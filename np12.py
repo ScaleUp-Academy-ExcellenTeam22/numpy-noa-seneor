2e59601ca23f9caa6e9f085894e299deac5a8ef5
noa-seneor <100723117+noa-seneor@users.noreply.github.com>
2022-05-31 12:56:07 +0200

    Update np12.py

--- np12.py
+++ np12.py
@@ -1,15 +1,9 @@
 import numpy as np
 
 
-def remove_single(shape):
+def remove_single(array_shape) -> tuple:
     """
-    Function that remove single-dimensional entries from a specified shape
+    Function that removes single-dimensional entries from a specified shape
     """
-    array = np.zeros(shape)
-    print(np.squeeze(array).shape)
-
-
-if __name__ == '__main__':
-    remove_single((3, 1, 4))
-
-
+    array = np.zeros(array_shape)
+    return (np.squeeze(array).shape)
