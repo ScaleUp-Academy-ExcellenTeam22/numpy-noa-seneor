fd6bd316f5b4274ac221d60a6ed132ddb8be7062
noa-seneor <100723117+noa-seneor@users.noreply.github.com>
2022-05-31 10:19:34 +0200

    Update np2.py

--- np2.py
+++ np2.py
@@ -1,14 +1,13 @@
 import numpy as np
 
 
-def np_random_vector():
+def np_random_vector() -> np.linspace:
     """
-    Function that creates a vector of length 10 with values
+    Function that returns a vector of length 10 with values
     evenly distributed between 5 and 50
-    :return: Vector
     """
-    vector = np.random.randint(5, 51, 10)
-    return vector
+    v = np.linspace(10, 49, 5)
+    return v
 
 
 if __name__ == '__main__':
